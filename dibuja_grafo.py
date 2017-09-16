f8248f01032e44c7fec1cd52e494372853ff0c10
Erick Fdo. Lopez Fimbres <y3rick_18@hotmail.com>
2017-09-16 15:14:34 -0700

    Funcion vecino aleatorio y conclusionesgit status!

--- dibuja_grafo.py
+++ dibuja_grafo.py
@@ -76,32 +76,32 @@ class problema_grafica_grafo(blocales.Problema):
         #genera 
         return tuple(random.randint(10, self.dim - 10) for _ in
                      range(2 * len(self.vertices)))
+                
+                #FUNCION DE VECINO ALEATORIO ANTERIOR
+    #def vecino_aleatorio(self, estado, dmax=10):
+    #    """
+    #    Encuentra un vecino en forma aleatoria. En esta primera
+    #    versión lo que hacemos es tomar un valor aleatorio, y
+    #    sumarle o restarle x pixeles al azar.
+    #
+    #    Este es un vecino aleatorio muy malo. Por lo que deberás buscar
+    #    como hacer un mejor vecino aleatorio y comparar las ventajas de
+    #    hacer un mejor vecino en el algoritmo de temple simulado.
 
-    def vecino_aleatorio(self, estado, dmax=10):
-        """
-        Encuentra un vecino en forma aleatoria. En esta primera
-        versión lo que hacemos es tomar un valor aleatorio, y
-        sumarle o restarle x pixeles al azar.
-
-        Este es un vecino aleatorio muy malo. Por lo que deberás buscar
-        como hacer un mejor vecino aleatorio y comparar las ventajas de
-        hacer un mejor vecino en el algoritmo de temple simulado.
-
-        @param estado: Una tupla con el estado.
-        @param dispersion: Un flotante con el valor de dispersión para el
-                           vertice seleccionado
-
-        @return: Una tupla con un estado vecino al estado de entrada.
-
-        """
-        vecino = list(estado)
-        i = random.randint(0, len(vecino) - 1)
+    #   @param estado: Una tupla con el estado.
+    #   @param dispersion: Un flotante con el valor de dispersión para el
+    #                      vertice seleccionado
+    
+    #   @return: Una tupla con un estado vecino al estado de entrada.
+    #
         
-        vecino[i] = max(10,min(self.dim - 10,
-                            vecino[i] + random.randint(-dmax,  dmax)))
-        print(vecino)
-        return tuple(vecino)
-
+    #   vecino = list(estado)
+    #   i = random.randint(0, len(vecino) - 1)   
+    #    vecino[i] = max(10,min(self.dim - 10,
+    #                        vecino[i] + random.randint(-dmax,  dmax)))
+    #    print(vecino)
+    #    return tuple(vecino)
+    
         #######################################################################
         #                          20 PUNTOS
         #######################################################################
@@ -109,18 +109,43 @@ class problema_grafica_grafo(blocales.Problema):
         #
         # Propon una manera alternativa de vecino_aleatorio y muestra que
         # con tu propuesta se obtienen resultados mejores o en menor tiempo
-    """def vecino_aleatorio(self, estado, dmax=10):
-        v = list(self.vertices)
-        #escojemos un vertice al azar
-        i = random.randint(0, len(v) - 1)
-        xD = math.ceil(random.uniform(-1, 1)*dmax)
-        yD = math.ceil(random.uniform(-1, 1)*dmax)
+    def vecino_aleatorio(self, estado, dmax=5):
+        """
+        Esta forma en que se genera el vecino ayuda a que encuentre una solucion mas
+        rápido
+        """
         vecino = list(estado)
-        vecino[i]=(xD if(vecino[i]+xD < self.dim-10) else self.dim-10)
-        vecino[i+1]=(yD if(vecino[i+1]+yD < self.dim-10) else self.dim-10)
+        #escojemos un vertice al azar
+        i = random.randint(0, len(vecino) - 1)
         
+        #volvemos a generar un vecino aleatorio en x,y
+        if i%2 == 0:#significa que cayo en un vertice en la componente x el valor de i
+            vecino[i]=random.randint(10, self.dim - 10)
+            vecino[i+1]=random.randint(10, self.dim - 10)
+        else:#sino entonces cayo en la componente y
+            vecino[i]=random.randint(10, self.dim - 10)
+            vecino[i-1]=random.randint(10, self.dim - 10)
+            
+        #Este metodo comentado no daba muy buenos resultados 
+        #hacia que el grafo se moviera a las esquinas y se miraba feo
+        #Mi plan era mover la componente x,y positiva o negativamente
+        #aleatoriamente de un vertice escogido al azar
+        """xD = math.ceil(random.uniform(-1, 1)*dmax)
+        yD = math.ceil(random.uniform(-1, 1)*dmax)
+        print("yD",yD,"xD",xD)
+        #print(i)
+        #print(len(vecino)-1)
+        if i%2 == 0:
+            #quiere decir que cayo un vertice en la componente y
+            vecino[i]=((vecino[i]+xD) if(vecino[i]+xD < self.dim-30 and vecino[i]+xD > 30) else vecino[i])
+            vecino[i+1]=(vecino[i]+yD if(vecino[i+1]+xD < self.dim-20 and vecino[i+1]+xD > 10) else vecino[i+1])
+        else:
+            #quiere decir que cayo un vertice en la componente y
+            vecino[i]=(vecino[i]+yD if(vecino[i]+xD < self.dim-30 and vecino[i]+xD > 30) else vecino[i])
+            vecino[i-1]=(vecino[i-1]+xD if(vecino[i-1]+xD < self.dim-30 and vecino[i-1]+xD > 30) else vecino[i-1])            
+        """
         return tuple(vecino)
-    """
+    
     def costo(self, estado):
         """
         Encuentra el costo de un estado. En principio el costo de un estado
@@ -288,14 +313,18 @@ class problema_grafica_grafo(blocales.Problema):
                     if(angulo <= 30):
                         total += 1-((angulo)/31)
                 except ZeroDivisionError:
-                    print("Division sobre 0")
+                    pass
+                    """para que no salga el mensaje de division muchas veces en pantalla
+                       lo comentamos
+                    """
+                    #print("Division sobre 0")
             
         # Agrega el método que considere el angulo entre aristas de
         # cada vertice. Dale diferente peso a cada criterio hasta
         # lograr que el sistema realice gráficas "bonitas"
         #
         # ¿Que valores de diste a K1, K2 y K3 respectivamente?
-        #
+        #K1=10 K2=3 y K3=7
         return total
 
     def criterio_propio(self, estado_dic):
@@ -316,9 +345,10 @@ class problema_grafica_grafo(blocales.Problema):
         #######################################################################
         # ¿Crees que hubiera sido bueno incluir otro criterio? ¿Cual?
         """
-        Si, uno que mide las distancias de las aristas para tener una cierta
-        medida base y el grafo se vea mas o menos parejo y asi no tenga 
-        aristas ni muy largas ni muy cortas
+        Si, mi criterio es uno que mide las distancias de las aristas para 
+        asi el grafo se vea mas o menos las aristas del mismo tamaño y asi no tenga 
+        aristas ni muy largas ni muy cortas haciendo que se vea visualmente
+        mas atractivo manteniendo un estandar en la longitud de aristas.
         """
         # Desarrolla un criterio propio y ajusta su importancia en el
         # costo total con K4 ¿Mejora el resultado? ¿En que mejora el
@@ -335,7 +365,6 @@ class problema_grafica_grafo(blocales.Problema):
             else:
                 total+= 1- longitudArista/d
         
-        #print("El total es: ",total)
         return total
 
     def estado2dic(self, estado):
@@ -416,14 +445,15 @@ def main():
 
     # Y vamos a hacer un dibujo del grafo sin decirle como hacer para
     # ajustarlo.
+    
     grafo_sencillo = problema_grafica_grafo(vertices_sencillo,
                                             aristas_sencillo,
                                             dimension)
     
-    """grafo_sencillo = problema_grafica_grafo(vertices_completo,
-                                            aristas_completo,
-                                            dimension)
-    """
+    #grafo_sencillo = problema_grafica_grafo(vertices_completo,
+    #                                        aristas_completo,
+    #                                        dimension)
+    
     
     estado_aleatorio = grafo_sencillo.estado_aleatorio()
     costo_inicial = grafo_sencillo.costo(estado_aleatorio)
@@ -442,7 +472,7 @@ def main():
     costo_final = grafo_sencillo.costo(solucion)
     
     grafo_sencillo.dibuja_grafo(solucion, "prueba_final.gif")
-    print("\nUtilizando la calendarización por default")
+    print("\nUtilizando la calendarización exponencial")
     print("Costo de la solución encontrada: {}".format(costo_final))
     print("Tiempo de ejecución en segundos: {}".format(t_final - t_inicial))
 
@@ -452,11 +482,24 @@ def main():
     # ¿Que valores para ajustar el temple simulado son los que mejor
     # resultado dan?
     """
-    Cambiar de calendarizador
+    Cambiar de calendarizador por defaul por uno exponencial, en promedio 
+    tardaba al rededor de 15-20 minutos para poder terminar de 
+    encontrar la solucion del grafo.
+    Ahora con la calendarizacion exponencial no pasa de los 2 -3 minutos
+    en promedio.
+    
     """
     # ¿Que encuentras en los resultados?, ¿Cual es el criterio mas importante?
     """
-    Los angulos
+    Es dificil de elegir el criterio mas importante, puesto que al 
+    definir que es bonito o no puede influir en la desicion,
+    en mi caso pienso que lo mas importante es el angulo del 
+    vertice. Pero a su vez tambien influye la distancia de los 
+    vertices es decir que no esten pegados, tambien entra el numero de cruces
+    aunque si cumple con estas dos el numero de cruces en algunos frafos
+    que estan totalmente conexos se ve bien aunque los tengan.
+    Tambien es importante el modo en que se generan los vecinos 
+    aleatorios ya que el tiempo y el costo de solicion se ve afectada.
     """
     # En general para obtener mejores resultados del temple simulado,
     # es necesario utilizar una función de calendarización acorde con
