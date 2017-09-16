3c0826fdeebb2f75090fd663d7922be627fbe473
Huguez <carlos.huguez@gmail.com>
2017-09-15 23:24:17 -0700

    tarea 2

--- blocales.py
+++ blocales.py
@@ -8,11 +8,10 @@ Algoritmos generales para búsquedas locales
 
 """
 
-__author__ = 'juliowaissman'
+__author__ = 'Carlos_Huguez'
 
 from itertools import takewhile
-from math import exp
-from math import log2
+import numpy as np
 from random import random
 
 
@@ -69,8 +68,7 @@ class Problema(object):
         """
         raise NotImplementedError("Este metodo debe ser implementado")
 
-
-def descenso_colinas(problema, maxit=1e6):
+def descenso_colinas( problema, maxit = 1e6 ):
     """
     Busqueda local por descenso de colinas.
 
@@ -80,19 +78,23 @@ def descenso_colinas(problema, maxit=1e6):
     @return: El estado con el menor costo encontrado
 
     """
-    estado = problema.estado_aleatorio()
-    costo = problema.costo(estado)
 
-    for _ in range(int(maxit)):
-        e = min(problema.vecinos(estado), key=problema.costo)
+    estado = problema.estado_aleatorio()
+    costo = problema.costo( estado )
+
+    for _ in range( int( maxit ) ):
+    
+        e = min( problema.vecinos( estado ), key = problema.costo )
         c = problema.costo(e)
+    
         if c >= costo:
             break
+        
         estado, costo = e, c
+    
     return estado
 
-
-def temple_simulado(problema, calendarizador=None, tol=0.001):
+def temple_simulado_exp( problema, calendarizador = None, tol = 0.001 ):
     """
     Busqueda local por temple simulado
 
@@ -104,21 +106,86 @@ def temple_simulado(problema, calendarizador=None, tol=0.001):
 
     """
     if calendarizador is None:
-        costos = [problema.costo(problema.estado_aleatorio())
-                  for _ in range(10 * len(problema.estado_aleatorio()))]
-        minimo,  maximo = min(costos), max(costos)
-        T_ini = 2 * (maximo - minimo)
-        calendarizador = (T_ini/(1 + i) for i in range(int(1e10)))
+        costos = [ problema.costo( problema.estado_aleatorio() ) for _ in range( 10 * len( problema.estado_aleatorio() ) ) ]
+        minimo,  maximo = min(costos), max( costos )
+        T_ini = 2 * ( maximo - minimo )
+        k = 0.001
+        calendarizador = ( T_ini * np.exp(-k*i) for i in range( int( 1e10 ) ) )
+
 
     estado = problema.estado_aleatorio()
-    costo = problema.costo(estado)
+    costo = problema.costo( estado )
 
-    for T in takewhile(lambda i: i > tol, calendarizador):
+    for T in takewhile( lambda i: i > tol, calendarizador ):
 
         vecino = problema.vecino_aleatorio(estado)
         costo_vecino = problema.costo(vecino)
         incremento_costo = costo_vecino - costo
 
-        if incremento_costo <= 0 or random() < exp(-incremento_costo / T):
+        if incremento_costo <= 0 or random() < np.exp( -incremento_costo / T ):
             estado, costo = vecino, costo_vecino
+
+    return estado
+
+def temple_simulado_log( problema, calendarizador = None, tol = 0.001 ):
+    """
+    Busqueda local por temple simulado
+
+    @param problema: Un objeto de la clase `Problema`.
+    @param calendarizador: Un generador de temperatura (simulación).
+    @param tol: Temperatura mínima considerada diferente a cero.
+
+    @return: El estado con el menor costo encontrado
+
+    """
+    if calendarizador is None:
+        costos = [ problema.costo( problema.estado_aleatorio() ) for _ in range( 10 * len( problema.estado_aleatorio() ) ) ]
+        minimo,  maximo = min(costos), max( costos )
+        T_ini = 2 * ( maximo - minimo )
+        calendarizador = ( T_ini / ( ( np.log( i + 1 ) ) + 1 )   for i in range( int( 1e10 ) ) )
+
+    estado = problema.estado_aleatorio()
+    costo = problema.costo( estado )
+
+    for T in takewhile( lambda i: i > tol, calendarizador ):
+
+        vecino = problema.vecino_aleatorio( estado )
+        costo_vecino = problema.costo( vecino )
+        incremento_costo = costo_vecino - costo
+
+        if incremento_costo <= 0 or random() < np.exp( -incremento_costo / T ):
+            estado, costo = vecino, costo_vecino
+
+    return estado
+
+def temple_simulado( problema, calendarizador = None, tol = 0.001 ):
+    """
+    Busqueda local por temple simulado
+
+    @param problema: Un objeto de la clase `Problema`.
+    @param calendarizador: Un generador de temperatura (simulación).
+    @param tol: Temperatura mínima considerada diferente a cero.
+
+    @return: El estado con el menor costo encontrado
+
+    """
+    if calendarizador is None:
+        costos = [ problema.costo( problema.estado_aleatorio() ) for _ in range( 10 * len( problema.estado_aleatorio() ) ) ]
+        minimo,  maximo = min(costos), max( costos )
+        T_ini = 2 * ( maximo - minimo )
+        calendarizador = ( T_ini / (i + 1) for i in range( int( 1e10 ) ) )
+        
+
+    estado = problema.estado_aleatorio()
+    costo = problema.costo( estado )
+
+    for T in takewhile( lambda i: i > tol, calendarizador ):
+
+        vecino = problema.vecino_aleatorio(estado)
+        costo_vecino = problema.costo(vecino)
+        incremento_costo = costo_vecino - costo
+
+        if incremento_costo <= 0 or random() < np.exp( -incremento_costo / T ):
+            estado, costo = vecino, costo_vecino
+    
     return estado
--- nreinas.py
+++ nreinas.py
@@ -8,10 +8,12 @@ Ejemplo de las n_reinas con búsquedas locales
 
 """
 
-__author__ = 'juliowaissman'
+__author__ = 'Carlos_Huguez'
 
 
 import blocales
+import time
+import numpy as np
 from random import shuffle
 from random import sample
 from itertools import combinations
@@ -30,9 +32,9 @@ class ProblemaNreinas(blocales.Problema):
         self.n = n
 
     def estado_aleatorio(self):
-        estado = list(range(self.n))
-        shuffle(estado)
-        return tuple(estado)
+        estado = list( range( self.n ) )
+        shuffle( estado )
+        return tuple( estado )
 
     def vecinos(self, estado):
         """
@@ -44,9 +46,13 @@ class ProblemaNreinas(blocales.Problema):
 
         """
         edo_lista = list(estado)
-        for i, j in combinations(range(self.n), 2):
+        
+        for i, j in combinations( range( self.n ), 2 ):
+
             edo_lista[i], edo_lista[j] = edo_lista[j], edo_lista[i]
-            yield tuple(edo_lista)
+            
+            yield tuple( edo_lista )
+            
             edo_lista[i], edo_lista[j] = edo_lista[j], edo_lista[i]
 
     def vecino_aleatorio(self, estado):
@@ -59,10 +65,13 @@ class ProblemaNreinas(blocales.Problema):
         @return: Una tupla con un estado vecino.
 
         """
-        vecino = list(estado)
-        i, j = sample(range(self.n), 2)
+        vecino = list( estado )
+        
+        i, j = sample( range(self.n), 2 )
+        
         vecino[i], vecino[j] = vecino[j], vecino[i]
-        return tuple(vecino)
+        
+        return tuple( vecino )
 
     def costo(self, estado):
         """
@@ -73,55 +82,64 @@ class ProblemaNreinas(blocales.Problema):
         @return: Un valor numérico, mientras más pequeño, mejor es el estado.
 
         """
-        return sum([1 for (i, j) in combinations(range(self.n), 2)
-                    if abs(estado[i] - estado[j]) == abs(i - j)])
+        return sum( [ 1 for ( i, j ) in combinations( range( self.n ), 2 ) if abs( estado[ i ] - estado[ j ] ) == abs( i - j ) ] )
 
 
-def prueba_descenso_colinas(problema=ProblemaNreinas(8), repeticiones=10):
+def prueba_descenso_colinas( problema = ProblemaNreinas( 8 ), repeticiones = 10 ):
     """ Prueba el algoritmo de descenso de colinas con n repeticiones """
 
-    print("\n\n" + "intento".center(10) +
-          "estado".center(60) + "costo".center(10))
+    print("\n\n" + "intento".center( 10 ) + "estado".center( 60 ) + "costo".center( 10 ) )
+    
     for intento in range(repeticiones):
-        solucion = blocales.descenso_colinas(problema)
-        print(str(intento).center(10) +
-              str(solucion).center(60) +
-              str(problema.costo(solucion)).center(10))
+        solucion = blocales.descenso_colinas( problema )
+        
+        print( str( intento ).center( 10 ) + str( solucion ).center( 60 ) + str( problema.costo( solucion ) ).center( 10 ) )
 
-
-def prueba_temple_simulado(problema=ProblemaNreinas(8)):
+def prueba_temple_simulado( problema = ProblemaNreinas( 8 ) ):
     """ Prueba el algoritmo de temple simulado """
 
-    solucion = blocales.temple_simulado(problema)
-    print("\n\nTemple simulado con calendarización To/(1 + i).")
-    print("Costo de la solución: ", problema.costo(solucion))
+    solucion = blocales.temple_simulado_exp( problema )
+
+    print("\n\nTemple simulado con calendarización To / ( log( i ) + 1 ).")
+    print("Costo de la solución: ", problema.costo( solucion ))
     print("Y la solución es: ")
-    print(solucion)
+    print( solucion )
 
 
 if __name__ == "__main__":
 
-    prueba_descenso_colinas(ProblemaNreinas(32), 10)
-    prueba_temple_simulado(ProblemaNreinas(32))
+	tiempo_ini = time.time()
+	reinas = 90
 
-    ##########################################################################
-    #                          20 PUNTOS
-    ##########################################################################
-    #
-    # ¿Cual es el máximo número de reinas que se puede resolver en
-    # tiempo aceptable con el método de 10 reinicios aleatorios?
-    #
-    # ¿Que valores para ajustar el temple simulado son los que mejor
-    # resultado dan? ¿Cual es el mejor ajuste para el temple simulado
-    # y hasta cuantas reinas puede resolver en un tiempo aceptable?
-    #
-    # En general para obtener mejores resultados del temple simulado,
-    # es necesario utilizarprobar diferentes metdos de
-    # calendarización, prueba al menos otros dis métodos sencillos de
-    # calendarización y ajusta los parámetros para que funcionen de la
-    # mejor manera
-    #
-    # Escribe aqui tus conclusiones
-    #
-    # ------ IMPLEMENTA AQUI TU CÓDIGO ---------------------------------------
-    #
+	prueba_descenso_colinas( ProblemaNreinas( reinas ), 10 )
+	prueba_temple_simulado( ProblemaNreinas( reinas ) )
+
+	tiempo_final = time.time()
+	print( "El tiempo final de ejecuacion fue: ", ( tiempo_final - tiempo_ini ) / 60.0 )
+	print
+
+	##########################################################################
+	#                          20 PUNTOS
+	##########################################################################
+	#
+	# ¿Cual es el máximo número de reinas que se puede resolver en
+	# tiempo aceptable con el método de 10 reinicios aleatorios?
+	#
+	# ¿Que valores para ajustar el temple simulado son los que mejor
+	# resultado dan? ¿Cual es el mejor ajuste para el temple simulado
+	# y hasta cuantas reinas puede resolver en un tiempo aceptable?
+	#
+	# En general para obtener mejores resultados del temple simulado,
+	# es necesario utilizarprobar diferentes metdos de
+	# calendarización, prueba al menos otros dis métodos sencillos de
+	# calendarización y ajusta los parámetros para que funcionen de la
+	# mejor manera
+	#
+	# Escribe aqui tus conclusiones
+	#
+	# ------ IMPLEMENTA AQUI TU CÓDIGO ---------------------------------------
+	#
+	# T0/i+1,  85 reinas,  42.20 minutos, 100 reinas 114.5434 minutos 
+	# T0*e^( -0.001 * i ),  90 reinas,  63.1261 minutos
+	# T0 / ( log( i ) + 1 ),  24 reinas,  60 minutos + 
+
