750680925fe1aa74bbed8f75b202f5926a38eca1
LizethSoto <soto.felix.liz@gmail.com>
2019-02-19 23:12:47 -0700

    :)

--- blocales.py
+++ blocales.py
@@ -14,6 +14,7 @@ from itertools import takewhile
 from math import exp
 from random import random
 
+
 class Problema(object):
     """
     Definición formal de un problema de búsqueda local. Es necesario
